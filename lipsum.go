fdf7e91f6daf65b325b52da84c884feb44e4de7e
Baiju Muthukadan <baiju.m.mail@gmail.com>
2016-02-03 19:30:58 +0530

    flush writer

--- lipsum.go
+++ lipsum.go
@@ -1,4 +1,49 @@
 package main
 
+import (
+	"io"
+	"log"
+	"net/http"
+	"time"
+)
+
+type flushWriter struct {
+	f http.Flusher
+	w io.Writer
+}
+
+func (fw *flushWriter) Write(p []byte) (n int, err error) {
+	n, err = fw.w.Write(p)
+	if fw.f != nil {
+		fw.f.Flush()
+	}
+	return
+}
+
+// ServeJunk return junk content
+func ServeJunk(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	fw := flushWriter{w: w}
+	if f, ok := w.(http.Flusher); ok {
+		fw.f = f
+	}
+
+	fw.Write([]byte("Hello"))
+	time.Sleep(4 * time.Second)
+	fw.Write([]byte("World"))
+
+}
+
 func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", ServeJunk)
+
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
